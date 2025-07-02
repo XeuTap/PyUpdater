3043499b66045ecbd382dbf9f9e697b4f05618a1
XeuTap <ctehkas@gmail.com>
2025-07-02 15:56:53 +0300

    Changed Version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ print(find_packages())
 
 setup(
     name="PyUpdater",
-    version="4.6.15",
+    version="4.6.16",
     description="Python Auto Update Library for Pyinstaller",
     long_description=readme,
     long_description_content_type="text/markdown",
